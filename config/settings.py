c0b52b737343acd945f022a8b3b9b6eddc67c511
User <tjasur@gmail.com>
2024-02-28 18:25:47 +0500

    add: car

--- config/settings.py
+++ config/settings.py
@@ -78,23 +78,23 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'allegro_db',
-        'HOST': '127.0.0.1',
-        'USER': 'allegro_user',
-        'PASSWORD': 'root',
-        'PORT': '3306', }
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'allegro_db',
+#         'HOST': '127.0.0.1',
+#         'USER': 'allegro_user',
+#         'PASSWORD': 'root',
+#         'PORT': '3306', }
+# }
 
 
 # Password validation
